4375eb90a8ac9f4702b7bf17301d2c45670bf584
Radugina <mariiapravda1990@gmail.com>
2025-05-10 22:52:56 +0300

    Add files via upload

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,12 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/dynamicid")
+
+button = driver.find_element(By.XPATH, "//button[text()='Button with Dynamic ID']")
+button.click()
+sleep(5)
+driver.quit()
---
+++ 05_lesson/lesson05_task4.py
@@ -0,0 +1,19 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("http://the-internet.herokuapp.com/login")
+
+username_field = driver.find_element(By.ID,"username")
+username_field.send_keys("tomsmith")
+
+password_field = driver.find_element(By.ID,"password")
+password_field.send_keys("SuperSecretPassword!")
+
+login_button = driver.find_element(By.XPATH,  "//button[type="radius"]')
+login_button.click()
+
+driver.quit()
