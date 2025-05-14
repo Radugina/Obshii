c9adf6aa0ae707f9635e2ae39ba988b4965496e0
Radugina <mariiapravda1990@gmail.com>
2025-05-14 21:43:56 +0300

    Add files via upload

---
+++ 06K_lesson/test_02_calc.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+delay = driver.find_element(By.CSS_SELECTOR, "input#delay")
+delay.clear()
+delay.send_keys("45")
+
+driver.find_element(By.XPATH, "//span[text()='7']").click()
+driver.find_element(By.XPATH, "//span[text()='+']").click()
+driver.find_element(By.XPATH, "//span[text()='8']").click()
+driver.find_element(By.XPATH, "//span[text()='=']").click()
+
+WebDriverWait(driver, 50).until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+    )
+result = driver.find_element(By.CSS_SELECTOR, ".screen").text
+assert int(result) == 15
+
+driver.quit()
