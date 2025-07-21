24357578d1b2e3e6836d82c6e8fc3a82010c2b87
Radugina <mariiapravda1990@gmail.com>
2025-07-21 19:39:47 +0300

    Delete 08_lesson/config.py

--- 08_lesson/config.py
+++
@@ -1,8 +0,0 @@
-API_BASE_URL = "https://ru.yougile.com/api-v2/"
-MY_CREDS = {
-            "login": 'mariiapravda1990@gmail.com',
-            "password": 'Ntcnbhjdobr1@',
-            "companyId": "736ee6f7-c9ba-471c-b028-f020915a22fb"
-}
-ADMIN = {"040ffab8-6e5c-4f4b-84ec-d0cbe18a66a1": "admin"}
-WORKER = {"040ffab8-6e5c-4f4b-84ec-d0cbe18a66a1": "worker"}
